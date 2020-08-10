d2df2d881360ecdd3cd79b7e91ff160e6461c11f
WojciechS <sankowski.wojciech@gmail.com>
2020-08-10 11:59:38 +0200

    Initial commit

---
+++ src/main/java/com/github/sankowskiwojciech/courseslessons/CoursesLessonsApplication.java
@@ -0,0 +1,13 @@
+package com.github.sankowskiwojciech.courseslessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesLessonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesLessonsApplication.class, args);
+	}
+
+}
